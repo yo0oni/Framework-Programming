c3ed7f8f1d5c1a8040850064f88f4efa3a19aca9
Kim Jiyoon <rlawldbs0911@naver.com>
2024-05-20 10:47:02 +0900

    7. AOP

--- aop/aop-java-gradle/lib/src/main/java/org/tukorea/web/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package org.tukorea.web.service;
-
-import org.tukorea.web.domain.StudentVO;
-
-public interface MemberService {
-	public StudentVO readMember(String userid) throws Exception;
-
-	public void addMember(StudentVO student) throws Exception;
-}
--- aop/aop-java/src/main/java/org/tukorea/web/persistence/MemberDAO.java
+++
@@ -1,9 +0,0 @@
-package org.tukorea.web.persistence;
-
-import org.tukorea.web.domain.StudentVO;
-
-public interface MemberDAO {
-	public StudentVO read(String id);
-
-	public void add(StudentVO student);
-}
